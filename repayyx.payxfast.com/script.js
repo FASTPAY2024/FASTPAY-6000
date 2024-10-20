16d87b7fed37695facb121c721f33854d80004d5
FAST PAY <mstaysab96@gmail.com>
2024-10-20 10:01:17 +0600

    Update script.js

--- repayyx.payxfast.com/script.js
+++ repayyx.payxfast.com/script.js
@@ -8,7 +8,7 @@ function copyAmount() {
 }
 
 function copyUPI() {
-    const upi = 'shayandeb45-2@okhdfcbank';
+    const upi = 'ramushil276-1@oksbi';
     navigator.clipboard.writeText(upi).then(() => {
         showPopup('copyModal', 'UPI copied to clipboard!');
     }).catch(err => {
